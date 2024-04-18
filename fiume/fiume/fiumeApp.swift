17250ab6955cae9f758bd920030ce6d4ce4beb63
Bill Wake <william.wake@acm.org>
2024-04-18 13:19:38 -0400

    Initial checkin of xcode-generated structures

---
+++ fiume/fiume/fiumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiumeApp.swift
+//  fiume
+//
+//  Created by Bill Wake on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct fiumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
