679a8f59a51e37448722ac73faf5dbd9fcad97cc
Bill Wake <william.wake@acm.org>
2024-05-13 16:21:03 -0400

    Pull subviews to their own file

--- fiume/fiume/ContentView.swift
+++ fiume/fiume/ContentView.swift
@@ -1,57 +1,6 @@
 import Charts
 import SwiftUI
 
-struct NumberField: View {
-	var label: String
-	@Binding var value: Int?
-
-	var body: some View {
-		LabeledContent {
-			TextField(label, value: $value, format: .number)
-				.keyboardType(.decimalPad)
-		} label: {
-			Text(label)
-		}
-	}
-}
-
-struct CreateStreamView: View {
-	@Environment(\.dismiss) var dismiss
-
-	@Bindable var plan: Plan
-
-	@State private var name = ""
-	@State private var amount: Int?
-	@State private var startMonth: Int?
-	@State private var endMonth: Int?
-
-	var body: some View {
-		Form {
-			LabeledContent {
-				TextField("Name", text: $name)
-			} label: {
-				Text("Name")
-			}
-			
-			NumberField(label: "Amount", value: $amount)
-
-			NumberField(label: "Start Month", value: $startMonth)
-
-			NumberField(label: "End Month", value: $endMonth)
-
-			Button("Create") {
-				plan.add(Stream(
-					name,
-					Dollar(amount ?? 0),
-					first: startMonth ?? 1,
-					last: endMonth))
-				dismiss()
-			}
-		}
-		.autocorrectionDisabled()
-	}
-}
-
 struct ContentView: View {
 	@Bindable var plan: Plan
 	@State var isPresentingSheet = false
---
+++ fiume/fiume/CreateStreamView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct CreateStreamView: View {
+	@Environment(\.dismiss) var dismiss
+
+	@Bindable var plan: Plan
+
+	@State private var name = ""
+	@State private var amount: Int?
+	@State private var startMonth: Int?
+	@State private var endMonth: Int?
+
+	var body: some View {
+		Form {
+			LabeledContent {
+				TextField("Name", text: $name)
+			} label: {
+				Text("Name")
+			}
+
+			NumberField(label: "Amount", value: $amount)
+
+			NumberField(label: "Start Month", value: $startMonth)
+
+			NumberField(label: "End Month", value: $endMonth)
+
+			Button("Create") {
+				plan.add(Stream(
+					name,
+					Dollar(amount ?? 0),
+					first: startMonth ?? 1,
+					last: endMonth))
+				dismiss()
+			}
+		}
+		.autocorrectionDisabled()
+	}
+}
