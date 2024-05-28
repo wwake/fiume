0cf066adcbd4b820a470a470df71afcce20b3863
Bill Wake <william.wake@acm.org>
2024-05-28 14:40:28 -0400

    Use signature shield for makeScenario

--- fiume/fiumeTests/AScenario.swift
+++ fiume/fiumeTests/AScenario.swift
@@ -12,65 +12,61 @@ final class AScenario: XCTestCase {
     return Stream(name, Money(amount), first: first, last: dateSpec)
   }
 
-  private func makeScenario() -> Scenario {
-    Scenario()
+  private func makeScenario(_ streams: fiume.Stream...) -> Scenario {
+    let result = Scenario()
+    streams.forEach {
+      result.add($0)
+    }
+    return result
   }
 
-	func test_makes_independent_copies() {
-		let sut = makeScenario()
-		let stream = makeStream("Income1", 1_000, first: 1, last: 12)
-		sut.add(stream)
+  func test_makes_independent_copies() {
+    let sut = makeScenario(makeStream("Income1", 1_000, first: 1, last: 12))
 
-		let result = sut.copy()
-		let stream2 = makeStream("Income2", 2_000, first: 1, last: 12)
-		result.add(stream2)
+    let result = sut.copy()
+    let stream2 = makeStream("Income2", 2_000, first: 1, last: 12)
+    result.add(stream2)
 
-		XCTAssertEqual(sut.net(1), Money(1_000))
-		XCTAssertEqual(result.net(1), Money(3_000))
-	}
+    XCTAssertEqual(sut.net(1), Money(1_000))
+    XCTAssertEqual(result.net(1), Money(3_000))
+  }
 
-	func test_built_for_one_stream() {
-		let sut = makeScenario()
-		let stream = makeStream("Income1", 1_000, first: 1, last: 12)
+  func test_built_for_one_stream() {
+    let sut = makeScenario(makeStream("Income1", 1_000, first: 1, last: 12))
 
-		sut.add(stream)
+    XCTAssertEqual(sut.net(12), Money(1_000))
+    XCTAssertEqual(sut.net(13), Money(0))
+  }
 
-		XCTAssertEqual(sut.net(12), Money(1_000))
-		XCTAssertEqual(sut.net(13), Money(0))
-	}
+  func test_built_for_distinct_streams() {
+    let sut = makeScenario(
+      makeStream("Income1", 1_000, first: 1, last: 12),
+      makeStream("Income2", 500, first: 10, last: nil)
+    )
 
-	func test_built_for_multiple_streams() {
-		let sut = makeScenario()
-		let stream1 = makeStream("Income1", 1_000, first: 1, last: 12)
-		let stream2 = makeStream("Income2", 500, first: 10, last: nil)
+    XCTAssertEqual(sut.net(1), Money(1_000))
+    XCTAssertEqual(sut.net(10), Money(1_500))
+    XCTAssertEqual(sut.net(12), Money(1_500))
+    XCTAssertEqual(sut.net(13), Money(500))
+  }
 
-		sut.add(stream1)
-		sut.add(stream2)
+  func test_built_for_merged_streams() {
+    let sut = makeScenario(
+      makeStream("Income1", 1_000, first: 1, last: 12),
+      makeStream("Income1", 500, first: 10, last: nil)
+    )
 
-		XCTAssertEqual(sut.net(1), Money(1_000))
-		XCTAssertEqual(sut.net(10), Money(1_500))
-		XCTAssertEqual(sut.net(12), Money(1_500))
-		XCTAssertEqual(sut.net(13), Money(500))
-	}
-
-	func test_built_for_merged_streams() {
-		let sut = makeScenario()
-		let stream1 = makeStream("Income1", 1_000, first: 1, last: 12)
-		let stream2 = makeStream("Income1", 500, first: 10, last: nil)
-
-		sut.add(stream1)
-		sut.add(stream2)
-
-		XCTAssertEqual(sut.net(1), Money(0))
-		XCTAssertEqual(sut.net(10), Money(500))
-		XCTAssertEqual(sut.net(12), Money(500))
-		XCTAssertEqual(sut.net(13), Money(0))
-	}
+    XCTAssertEqual(sut.net(1), Money(0))
+    XCTAssertEqual(sut.net(10), Money(500))
+    XCTAssertEqual(sut.net(12), Money(500))
+    XCTAssertEqual(sut.net(13), Money(0))
+  }
 
   func test_salary_minus_expenses_creates_net_worth() throws {
-    let sut = makeScenario()
-    sut.add(makeStream("Salary", Money(1_000)))
-    sut.add(makeStream("Expenses", Money(-900)))
+    let sut = makeScenario(
+      makeStream("Salary", Money(1_000)),
+      makeStream("Expenses", Money(-900))
+    )
 
     let result = sut.project(1...12)
 
