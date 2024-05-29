e867add190a1b7dfbaf94aa84a35fd7a2e60ddb1
Bill Wake <william.wake@acm.org>
2024-05-29 17:00:23 -0400

    Create MonthYearView, two scroll wheels for Year and Month

--- fiume/Sources/view/MonthYearView.swift
+++ fiume/Sources/view/MonthYearView.swift
@@ -3,12 +3,44 @@ import SwiftUI
 struct MonthYearView: View {
   @Binding var monthYear: MonthYear?
 
+  @State private var monthNumber = 0
+  @State private var yearNumber = 2000
+
+  private var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  private var years = 1920..<2101
+
+  init(monthYear: Binding<MonthYear?>) {
+    self._monthYear = monthYear
+  }
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    GeometryReader { geometry in
+      HStack {
+        Picker("Year", selection: self.$yearNumber) {
+          ForEach(years, id: \.self) { year in
+            Text(verbatim: "\(year)")
+              .tag(year)
+          }
+        }
+        .pickerStyle(WheelPickerStyle())
+        .frame(width: geometry.size.width / 2, height: geometry.size.height)
+        .clipped()
+
+        Picker("Month", selection: self.$monthNumber) {
+          ForEach(0..<months.count, id: \.self) { monthIndex in
+            Text(verbatim: "\(months[monthIndex])")
+              .tag(monthIndex)
+          }
+        }
+        .pickerStyle(WheelPickerStyle())
+        .frame(width: geometry.size.width / 2, height: geometry.size.height)
+        .clipped()
+      }
+    }
   }
 }
 
 #Preview {
-  @State var monthYear: MonthYear? = MonthYear(month: 2, year: 2025)
-  return MonthYearView(monthYear: $monthYear)
+  @State var myMonthYear: MonthYear? = MonthYear(month: 2, year: 2025)
+  return MonthYearView(monthYear: $myMonthYear)
 }
