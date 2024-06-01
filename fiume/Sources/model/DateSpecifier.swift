56f1504bdacd6ca03b94e25da5ae122e8d1c6250
Bill Wake <william.wake@acm.org>
2024-06-01 15:37:08 -0400

    Remove dead code

--- fiume/Sources/model/DateSpecifier.swift
+++ fiume/Sources/model/DateSpecifier.swift
@@ -1,26 +1,4 @@
 enum DateSpecifier: Equatable {
   case unspecified
   case month(Int)
-
-  func asOptionalInt() -> Int? {
-    switch self {
-    case .unspecified:
-      return nil
-
-    case .month(let monthNumber):
-      return monthNumber
-    }
-  }
-}
-
-extension DateSpecifier: CustomDebugStringConvertible {
-  var debugDescription: String {
-    switch self {
-    case .unspecified:
-      return "?"
-
-    case .month(let monthNumber):
-      return "\(monthNumber)"
-    }
-  }
 }
