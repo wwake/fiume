e587979e13b88b7924158db799881a5e51af94f2
Bill Wake <william.wake@acm.org>
2024-06-18 21:05:17 -0400

    Improve error handling on delete all

--- fiume/Sources/view/ContentView.swift
+++ fiume/Sources/view/ContentView.swift
@@ -25,17 +25,17 @@ struct ContentView: View {
     NavigationStack {
       Text("Net Worth")
         .font(.title)
-//      Chart(possibilities.netWorth(possibilities.range(numberOfMonths))) { dataSeries in
-//        ForEach(dataSeries.netWorthByMonth) {
-//          LineMark(
-//            x: .value("Month", $0.month),
-//            y: .value("Net Worth", $0.amount)
-//          )
-//        }
-//        .foregroundStyle(by: .value("Scenario Name", dataSeries.name))
-//      }
-//      .chartXScale(domain: possibilities.range(numberOfMonths))
-//      .padding()
+      //      Chart(possibilities.netWorth(possibilities.range(numberOfMonths))) { dataSeries in
+      //        ForEach(dataSeries.netWorthByMonth) {
+      //          LineMark(
+      //            x: .value("Month", $0.month),
+      //            y: .value("Net Worth", $0.amount)
+      //          )
+      //        }
+      //        .foregroundStyle(by: .value("Scenario Name", dataSeries.name))
+      //      }
+      //      .chartXScale(domain: possibilities.range(numberOfMonths))
+      //      .padding()
 
       HStack {
         Button("People") {
@@ -49,14 +49,18 @@ struct ContentView: View {
         .padding(.leading, 20)
 
         Button("Clear All Data", role: .destructive) {
-          // swiftlint:disable:next force_try
-          try! modelContext.delete(model: Person.self)
+          do {
+            try modelContext.delete(model: Person.self)
 
-          // swiftlint:disable:next force_try
-          try! modelContext.delete(model: Plan.self)
+            try modelContext.delete(model: Plan.self)
 
-          modelContext.insert(Plan.makeAnd("My Finances"))
-          try? modelContext.save()
+            modelContext.insert(Plan.makeAnd("My Finances"))
+
+            try modelContext.save()
+          } catch {
+            print(error.localizedDescription)
+            fatalError("Couldn't delete all - \(#file)")
+          }
         }
         .padding(12)
         .background(Color.red)
@@ -70,10 +74,10 @@ struct ContentView: View {
       PlanListView(possibilities: Possibilities(startDate: startDate, plans: plans))
     }
     .onAppear {
-//      if plans.isEmpty {
-//        modelContext.insert(Plan.makeAnd("My Finances"))
-//        try? modelContext.save()
-//      }
+      //      if plans.isEmpty {
+      //        modelContext.insert(Plan.makeAnd("My Finances"))
+      //        try? modelContext.save()
+      //      }
     }
     .sheet(isPresented: $isShowingPeople) {
       PeopleView()
@@ -84,9 +88,9 @@ struct ContentView: View {
 #Preview {
   let container = demoContainer(for: Plan.self)
 
-//  let possibilities = Possibilities(startDate: MonthYear(date: Date()), plans: plans)
-//  possibilities.add(Stream("Salary", 1_000, first: .month(2024.jan), last: .month(2029.jan)))
-//  possibilities.add(Stream("Expenses", -800, first: .month(2024.jan), last: .unchanged))
+  //  let possibilities = Possibilities(startDate: MonthYear(date: Date()), plans: plans)
+  //  possibilities.add(Stream("Salary", 1_000, first: .month(2024.jan), last: .month(2029.jan)))
+  //  possibilities.add(Stream("Expenses", -800, first: .month(2024.jan), last: .unchanged))
   return ContentView(startDate: MonthYear(date: Date()))
     .modelContainer(container)
 }
