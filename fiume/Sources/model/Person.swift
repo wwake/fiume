1bfdc0d0e89e526d56ab95c970fb2a921f1129a5
Bill Wake <william.wake@acm.org>
2024-07-01 14:18:40 -0400

    Make Person.id able to be included in constructor (for persistence)

--- fiume/Sources/model/Person.swift
+++ fiume/Sources/model/Person.swift
@@ -2,12 +2,13 @@ import Foundation
 import SwiftUI
 
 struct Person: Identifiable, Equatable, Codable {
-  var id = UUID()
+  var id: UUID
   let name: String
   let birth: MonthYear
   let death: MonthYear?
 
-  init(name: String, birth: MonthYear, death: MonthYear?) {
+  init(_ id: UUID = UUID(), name: String, birth: MonthYear, death: MonthYear?) {
+    self.id = id
     self.name = name
     self.birth = birth
     self.death = death
