d9c4412abf804837956ff469c5b24dc37c8442b8
Bill Wake <william.wake@acm.org>
2024-07-29 15:38:42 -0400

    Make AmountView able to work with both cases of Amount (no relative view yet)

--- fiume/Sources/view/AmountView.swift
+++ fiume/Sources/view/AmountView.swift
@@ -29,11 +29,64 @@ struct PoolAmountView: View {
   }
 }
 
+enum AmountType: String, CaseIterable, Identifiable {
+  var id: Self { self }
+
+  case money = "Money",
+       relative = "Relative"
+}
+
+struct MoneyAmountView: View {
+  var isIncome: Bool
+  var positiveColor: String
+  var negativeColor: String
+
+  @Binding var amount: Amount
+
+  @Binding var dollars: Int
+
+  init(
+    isIncome: Bool,
+    positiveColor: String,
+    negativeColor: String,
+    amount: Binding<Amount>,
+    dollars: Binding<Int>
+  ) {
+    self.isIncome = isIncome
+    self.positiveColor = positiveColor
+    self.negativeColor = negativeColor
+
+    self._amount = .init(projectedValue: amount)
+
+    self._dollars = dollars
+  }
+
+  fileprivate var backgroundColor: Color {
+    if dollars <= 0 { return Color("Neutral") }
+    return isIncome ? Color("Asset") : Color("Liability")
+  }
+
+  var body: some View {
+    VStack {
+      NumberField(label: "Amount $", value: $dollars)
+        .padding(2)
+        .background(backgroundColor)
+
+      if dollars < 0 {
+        Text("Amount may not be negative; choose Asset or Liability instead.")
+          .foregroundStyle(Color.red)
+      }
+    }
+  }
+}
+
 struct AmountView: View {
   var isIncome: Bool
   var positiveColor: String
   var negativeColor: String
   @Binding var amount: Amount
+
+  @State var amountType: AmountType = .money
 
   @State var dollars: Int
 
@@ -47,26 +100,47 @@ struct AmountView: View {
     self.positiveColor = positiveColor
     self.negativeColor = negativeColor
     self._amount = .init(projectedValue: amount)
-    self.dollars = abs(Int(amount.wrappedValue.value()))
-  }
 
-  fileprivate var backgroundColor: Color {
-    if dollars <= 0 { return Color("Neutral") }
-    return isIncome ? Color("Asset") : Color("Liability")
+    switch amount.wrappedValue {
+    case .money(let dollars):
+      self.dollars = dollars
+      amountType = .money
+
+    case let .relative(ratio, stream):
+      self.dollars = 0
+      amountType = .relative
+
+    @unknown default:
+      fatalError("Amount View unknown type \(amount.wrappedValue)")
+    }
   }
 
   var body: some View {
     VStack {
-      NumberField(label: "Amount $", value: $dollars)
-        .padding(2)
-        .background(backgroundColor)
-      if dollars < 0 {
-        Text("Amount may not be negative; choose Asset or Liability instead.")
-          .foregroundStyle(Color.red)
+      HStack {
+        Text("Choose Amount")
+        Spacer()
       }
-    }
-    .onChange(of: dollars) {
-      amount = Amount.money(dollars)
+
+      Picker("Add New", selection: $amountType) {
+        ForEach(AmountType.allCases) {
+          Text($0.rawValue.capitalized)
+        }
+      }
+      .pickerStyle(.segmented)
+      //      .onChange(of: dateType) { _, newValue in
+      //        updateDateSpec(newValue)
+      //      }
+
+      switch amountType {
+      case .money:
+        MoneyAmountView(isIncome: isIncome, positiveColor: positiveColor, negativeColor: negativeColor, amount: $amount, dollars: $dollars)
+          .frame(height: 100)
+
+      case .relative:
+        EmptyView()
+          .frame(height: 100)
+      }
     }
   }
 }
