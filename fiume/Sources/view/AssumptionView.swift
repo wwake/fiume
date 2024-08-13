dd6db624af75fc0376b88f7166a42097d201060e
Bill Wake <william.wake@acm.org>
2024-08-13 16:22:50 -0400

    Added delete button to AssumptionView- not active yet

--- fiume/Sources/view/AssumptionView.swift
+++ fiume/Sources/view/AssumptionView.swift
@@ -3,13 +3,20 @@ import SwiftUI
 
 struct AssumptionView: View {
   var assumption: Assumption
-  var action: (Assumption) -> Void
+  var updateAction: (Assumption) -> Void
+  var deleteAction: (String) -> Void
 
   @State var value: Double
 
-  init(assumption: Assumption, action: @escaping (Assumption) -> Void) {
+  init(
+    assumption: Assumption,
+    updateAction: @escaping (Assumption) -> Void = { _ in },
+    deleteAction: @escaping (String) -> Void = { _ in }
+  ) {
     self.assumption = assumption
-    self.action = action
+    self.updateAction = updateAction
+    self.deleteAction = deleteAction
+
     self.value = Double(assumption.current)
   }
 
@@ -23,16 +30,25 @@ struct AssumptionView: View {
         in: Double(assumption.min)...Double(assumption.max),
         onEditingChanged: { _ in
           if assumption.current != Int(value) {
-            action(Assumption(assumption, Int(value)))
+            updateAction(Assumption(assumption, Int(value)))
           }
         }
       )
       .onChange(of: value) { }
+
+      Spacer()
+      Button(action: {
+        deleteAction(assumption.name)
+      }) {
+        Image(systemName: "trash")
+          .accessibilityLabel(Text("Delete"))
+      }
+      .buttonStyle(.plain)
     }
   }
 }
 
 #Preview {
   let assumption = Assumption(type: AssumptionType.percent, name: "ROI", min: 0, max: 20, current: 5)
-  return AssumptionView(assumption: assumption) { _ in }
+  return AssumptionView(assumption: assumption, deleteAction: { _ in })
 }
