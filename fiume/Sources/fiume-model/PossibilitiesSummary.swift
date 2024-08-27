17183cf7cd39c2e5ff9d83a7d3727f94bd93a7bc
Bill Wake <william.wake@acm.org>
2024-08-27 16:23:52 -0400

    Clicking on a net income thumbnail takes you to a full-sized view

--- fiume/Sources/fiume-model/PossibilitiesSummary.swift
+++ fiume/Sources/fiume-model/PossibilitiesSummary.swift
@@ -6,6 +6,16 @@ public struct ScenarioSummary: Identifiable {
   public let netWorthByMonth: [MonthlySummary]
 }
 
+extension ScenarioSummary: Hashable {
+  public static func == (lhs: ScenarioSummary, rhs: ScenarioSummary) -> Bool {
+    lhs.id == rhs.id
+  }
+
+  public func hash(into hasher: inout Hasher) {
+      hasher.combine(id)
+  }
+}
+
 public struct MonthlySummary: Identifiable {
   public let id = UUID()
   public let month: MonthYear
--- fiume/Sources/view/MultiNetIncomeView.swift
+++ fiume/Sources/view/MultiNetIncomeView.swift
@@ -5,27 +5,44 @@ public struct MultiNetIncomeView: View {
   private var numberOfMonths: Int
   private var data: PossibilitiesSummary
 
+  @State private var showDetail = false
+  @State private var name = ""
+  @State private var detail: ScenarioSummary?
+
   public init(numberOfMonths: Int, data: PossibilitiesSummary) {
     self.numberOfMonths = numberOfMonths
     self.data = data
   }
 
   private let sizes = [
-    GridItem(.adaptive(minimum: 200), spacing: 40),
+    GridItem(.adaptive(minimum: 200, maximum: 200), spacing: 40),
   ]
 
   public var body: some View {
-    ScrollView {
-      LazyVGrid(columns: sizes, spacing: 25) {
-        ForEach(data) { scenario in
-          VStack {
-            Text(scenario.name)
-              .font(.caption)
-            NetIncomeView(numberOfMonths: numberOfMonths, scenario: scenario)
+    NavigationStack {
+      ScrollView {
+        LazyVGrid(columns: sizes, spacing: 25) {
+          ForEach(data) { scenario in
+            NavigationLink(value: scenario) {
+              VStack {
+                Text(scenario.name)
+                  .font(.caption)
+                NetIncomeView(numberOfMonths: numberOfMonths, scenario: scenario)
+              }
+            }
+            .frame(maxWidth: 200)
           }
         }
+        .padding()
       }
-      .padding()
+    }
+    .navigationDestination(for: ScenarioSummary.self) { summary in
+      VStack {
+        Text("Net Income By Month")
+          .font(.title)
+        Text(summary.name)
+        NetIncomeView(numberOfMonths: numberOfMonths, scenario: summary)
+      }.padding(12)
     }
   }
 }
