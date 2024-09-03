d83d95f43dcb068c3333dbe51999dd2f2602dff9
Bill Wake <william.wake@acm.org>
2024-09-03 15:45:06 -0400

    Rename ThePercentAssumptions to TheAssumptions; move tests from TheDateAssumptions

--- fiume/Tests/TheAssumptions.swift
+++ fiume/Tests/TheAssumptions.swift
@@ -2,7 +2,19 @@
 import fiume_model
 import Testing
 
-struct ThePercentAssumptions {
+struct TheAssumptions {
+  func makeAssumptions() -> Assumptions {
+    Assumptions()
+  }
+
+  func makeEmptyAssumptions() -> Assumptions {
+    let assumptions = makeAssumptions()
+    Array(assumptions).forEach {
+      assumptions.remove($0.name)
+    }
+    return assumptions
+  }
+
   @Test
   func adds_an_assumption() {
     let sut = Assumptions()
@@ -86,5 +98,46 @@ struct ThePercentAssumptions {
 
     #expect(assumptions.findMonthlyRate("ROI") >= 1.059463094)
     #expect(assumptions.findMonthlyRate("ROI") <= 1.059463095)
+  }
+
+  @Test
+  func knows_current_date_when_found() {
+    let sut = makeAssumptions()
+    let assumption = Assumption(type: .date, name: "Holiday", min: 2020, max: 2030, current: 2025)
+    sut.add(assumption)
+
+    #expect(sut.current("Holiday") == 2025.jan)
+  }
+
+  @Test
+  func defaults_current_date_when_not_found() {
+    let sut = makeAssumptions()
+
+    #expect(sut.current("Missing") == 1900.jan)
+  }
+
+  @Test
+  func first_returns_name_alphabetically_if_non_empty() {
+    let sut = makeEmptyAssumptions()
+    sut.add(Assumption(type: .date, name: "Z", min: 0, max: 10, current: 5))
+    sut.add(Assumption(type: .date, name: "A", min: 0, max: 10, current: 9))
+
+    #expect(sut.firstName == "A")
+  }
+
+  @Test
+  func first_returns_nil_if_empty() {
+    let sut = makeEmptyAssumptions()
+
+    #expect(sut.firstName == nil)
+  }
+
+  @Test
+  func names_returns_alphabetical_list() {
+    let sut = makeEmptyAssumptions()
+    sut.add(Assumption(type: .date, name: "Z", min: 0, max: 10, current: 5))
+    sut.add(Assumption(type: .date, name: "A", min: 0, max: 10, current: 9))
+
+    #expect(sut.names == ["A", "Z"])
   }
 }
--- fiume/Tests/TheDateAssumptions.swift
+++ fiume/Tests/TheDateAssumptions.swift
@@ -16,42 +16,6 @@ struct TheDateAssumptions {
   }
 
   @Test
-  func adds_new_assumptions() {
-    let sut = makeAssumptions()
-    sut.wasChanged = false
-
-    sut.add(Assumption(type: .date, name: "Holiday", min: 2020, max: 2030, current: 2025))
-
-    #expect(sut.wasChanged)
-    #expect(sut.find("Holiday") != nil)
-  }
-
-  @Test
-  func remove_assumption() {
-    let sut = makeAssumptions()
-    sut.add(Assumption(type: .date, name: "Holiday", min: 2020, max: 2030, current: 2025))
-    sut.wasChanged = false
-
-    sut.remove("Holiday")
-
-    #expect(sut.wasChanged)
-    #expect(sut.find("the one") == nil)
-  }
-
-  @Test
-  func replaces_an_assumption() {
-    let sut = makeAssumptions()
-    let assumption = Assumption(type: .date, name: "Holiday", min: 2020, max: 2030, current: 2025)
-    sut.add(assumption)
-    sut.wasChanged = false
-
-    sut.replace(Assumption(assumption, 2027))
-
-    #expect(sut.wasChanged)
-    #expect(sut.find("Holiday")!.current == 2027)
-  }
-
-  @Test
   func knows_current_date_when_found() {
     let sut = makeAssumptions()
     let assumption = Assumption(type: .date, name: "Holiday", min: 2020, max: 2030, current: 2025)
